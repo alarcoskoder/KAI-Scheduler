bdcffa5c7a279f0af201f58945bac9170ca48959
balazs.finta <balazs.finta@aimotive.com>
2025-12-09 16:46:27 +0100

    fix missing changes

--- cmd/podgroupcontroller/app/options.go
+++ cmd/podgroupcontroller/app/options.go
@@ -33,7 +33,7 @@ func InitOptions(fs *flag.FlagSet) *Options {
 		"The address the metric endpoint binds to.")
 	fs.StringVar(&options.ProbeAddr, "health-probe-bind-address", ":8081",
 		"The address the probe endpoint binds to.")
-	flag.BoolVar(&options.EnableLeaderElection, "leader-elect", true,
+	fs.BoolVar(&options.EnableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	fs.BoolVar(&options.SkipControllerNameValidation, "skip-controller-name-validation", false, "Skip controller name validation.")
